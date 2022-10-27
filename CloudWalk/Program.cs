0498d000a04d5408297bbfe2de150085732292a0
alber <alber@DESKTOP-3TDN679>
2022-10-27 18:05:20 -0400

    Add project files.

---
+++ CloudWalk/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using System.Diagnostics;
+
+namespace CloudWalkProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Write("Hello, World!");
+            ProcessLog.Process(@"C:\Users\alber\source\repos\CloudWalk\CloudWalk\Log\qgames.log");
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ LogParser/ProcessLog.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace CloudWalkProject
+{
+    public class ProcessLog
+    {
+        public static void Process(string filePath)
+        {
+            if (File.Exists(filePath))
+                ReadLog(filePath);
+        }
+        public static void ReadLog(string filePath)
+        {
+            string data;
+            StreamReader streamReader = null;
+            try
+            {
+                // Read the log
+                streamReader = new StreamReader(filePath);
+                data = streamReader.ReadLine();
+
+                while (streamReader != null)
+                {
+                    Debug.Write(data);
+                    data = streamReader.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write(e.Message);
+            }
+            finally
+            {
+                streamReader.Close();
+            }
+        }
+        public static void LogParser(string filePath)
+        {
+            
+            
+        }
+    }
+
+    
+
+
+}
